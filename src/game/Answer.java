09021ef5d51ff215fcea69950678be1f191664c4
Jonas Tjahjadi <jonastjahjadi@gmail.com>
2019-01-09 12:23:11 -0500

    Answer class wasn't staged in eclipse

---
+++ src/game/Answer.java
@@ -0,0 +1,20 @@
+package game;
+
+public class Answer {
+
+	private String answer;
+	private int val;
+	
+	public Answer(String answer, int val) {
+		this.answer = answer;
+		this.val = val;
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+	public int getVal() {
+		return val;
+	}
+}
